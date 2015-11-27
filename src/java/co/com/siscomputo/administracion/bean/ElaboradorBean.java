3e13f890cf39a743a54065ad51d5a05788ce75e8
lenovo <LENOVO@LENOVO-PC>
2015-11-27 08:46:15 -0500

    27/11/2015 0009
    Commit Mañanaero

--- src/java/co/com/siscomputo/administracion/bean/ElaboradorBean.java
+++ src/java/co/com/siscomputo/administracion/bean/ElaboradorBean.java
@@ -1,14 +1,264 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.com.siscomputo.administracion.bean;
+
+import co.com.siscomputo.administracion.logic.ElaboradorLogic;
+import co.com.siscomputo.administracion.logic.UsuarioLogic;
+import co.com.siscomputo.endpoint.ElaboradorEntity;
+import co.com.siscomputo.endpoint.MenuPermisosEntity;
+import co.com.siscomputo.endpoint.UsuarioEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author LENOVO
  */
-public class ElaboradorBean {
-    
+@ManagedBean(name = "Elaborador")
+@ViewScoped
+public class ElaboradorBean implements Serializable {
+
+    private ArrayList<ElaboradorEntity> lista;
+    private ArrayList<ElaboradorEntity> listaFiltro;
+    private ElaboradorEntity objetoElaborador;
+    private boolean ingresar;
+    private boolean actualizar;
+    private boolean eliminar;
+
+    public ArrayList<ElaboradorEntity> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<ElaboradorEntity> lista) {
+        this.lista = lista;
+    }
+
+    public ArrayList<ElaboradorEntity> getListaFiltro() {
+        return listaFiltro;
+    }
+
+    public void setListaFiltro(ArrayList<ElaboradorEntity> listaFiltro) {
+        this.listaFiltro = listaFiltro;
+    }
+
+    public ElaboradorEntity getObjetoElaborador() {
+        return objetoElaborador;
+    }
+
+    public void setObjetoElaborador(ElaboradorEntity objetoElaborador) {
+        this.objetoElaborador = objetoElaborador;
+    }
+
+    public boolean isIngresar() {
+        return ingresar;
+    }
+
+    public void setIngresar(boolean ingresar) {
+        this.ingresar = ingresar;
+    }
+
+    public boolean isActualizar() {
+        return actualizar;
+    }
+
+    public void setActualizar(boolean actualizar) {
+        this.actualizar = actualizar;
+    }
+
+    public boolean isEliminar() {
+        return eliminar;
+    }
+
+    public void setEliminar(boolean eliminar) {
+        this.eliminar = eliminar;
+    }
+
+    @PostConstruct
+    public void init() {
+        consultarElaborador();
+        permisos();
+    }
+
+    public ElaboradorBean() {
+        objetoElaborador = new ElaboradorEntity();
+        objetoElaborador.setUsuarioElaborador(new UsuarioEntity());
+    }
+
+    /**
+     * Método que tra una lista de Aprobador Elaborador
+     */
+    public void consultarElaborador() {
+        try {
+            ElaboradorLogic elaboradorLogic = new ElaboradorLogic();
+            lista = elaboradorLogic.listaElaborador();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Método que permite insertar un Aprobador Elaborador nuevo
+     */
+    public void instertarElaborador() {
+        try {
+            ElaboradorLogic elaboradorLogic = new ElaboradorLogic();
+            ElaboradorEntity elaboradorEntity = elaboradorLogic.insertarElaborador(objetoElaborador);
+            FacesMessage msg = null;
+            if (elaboradorEntity != null) {
+                msg = new FacesMessage("", "inserción de Aprobador Elaborador correcto");
+                adicionarMetodoPtoteccionLista(objetoElaborador);
+            } else {
+                msg = new FacesMessage("", "inserción de Aprobador Elaborador incorrecto");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Método que añade un Aprobador Elaborador visualmente
+     *
+     * @param objetoElaborador
+     */
+    private void adicionarMetodoPtoteccionLista(ElaboradorEntity objetoElaborador) {
+        lista.add(objetoElaborador);
+        UsuarioLogic usuarioLogic=new UsuarioLogic();
+        UsuarioEntity usuarioEntity = usuarioLogic.usuarioPorID(objetoElaborador.getUsuarioElaborador().getIdUsuario());
+        objetoElaborador.setUsuarioElaborador(usuarioEntity);        
+        lista.add(objetoElaborador);
+    }
+
+    /**
+     * Método que permite actualizar un Aprobador Elaborador
+     */
+    public void actualizarElaborador() {
+        ElaboradorLogic metodoRecuperacionLogic = new ElaboradorLogic();
+        String valida = metodoRecuperacionLogic.actualizarElaborador(objetoElaborador);
+        FacesMessage msg = null;
+        if ("Ok".equalsIgnoreCase(valida)) {
+            msg = new FacesMessage("", "actualización de Aprobador Elaborador correcto");
+            actualizarElaboradorLista(objetoElaborador);
+        } else {
+            msg = new FacesMessage("", "actualización de Aprobador Elaborador incorrecto");
+        }
+        nuevoElaboradorObjeto();
+        RequestContext.getCurrentInstance().execute("PF('actualizarElaborador').hide()");
+    }
+
+    /**
+     * Método que actualiza visualmente la lista de Aprobador Elaborador
+     *
+     * @param objetoElaborador
+     */
+    private void actualizarElaboradorLista(ElaboradorEntity objetoElaborador) {
+        try {
+            ArrayList<ElaboradorEntity> listaaux = new ArrayList<>();
+            if (lista != null) {
+                for (ElaboradorEntity item : lista) {
+                    int v1 = objetoElaborador.getIdElaborador();
+                    int v2 = item.getIdElaborador();
+                    if (v1 == v2) {
+                        UsuarioLogic usuarioLogic = new UsuarioLogic();
+                        UsuarioEntity usuarioEntity = usuarioLogic.usuarioPorID(objetoElaborador .getUsuarioElaborador().getIdUsuario());
+                        objetoElaborador.setUsuarioElaborador(usuarioEntity);
+                        listaaux.add(objetoElaborador);
+                    } else {
+                        listaaux.add(item);
+                    }
+                }
+            }
+            this.lista = new ArrayList<>();
+            this.lista = listaaux;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Método que se invoca al seleccionar una fila de la tabla
+     *
+     * @param event
+     */
+    public void onRowSelect(SelectEvent event) {
+        objetoElaborador = (ElaboradorEntity) event.getObject();
+        if (objetoElaborador.getUsuarioElaborador()== null) {
+            UsuarioEntity usuarioEntity = new UsuarioEntity();
+            usuarioEntity.setIdUsuario(-1);
+            objetoElaborador.setUsuarioElaborador(usuarioEntity);
+        }
+    }
+
+    /**
+     * Método que elimina un Aprobador Elaborador
+     */
+    public void eliminarElaborador() {
+        ElaboradorLogic metodoRecuperacionLogic = new ElaboradorLogic();
+        objetoElaborador.setEstadoElaborador("E");
+        metodoRecuperacionLogic.actualizarElaborador(objetoElaborador);
+        eliminarElaboradorLista(objetoElaborador);
+        RequestContext.getCurrentInstance().execute("PF('actualizarElaborador').hide()");
+        nuevoElaboradorObjeto();
+    }
+
+    /**
+     * Método que elimina visualmente un objeto de la lista
+     *
+     * @param objetoElaborador
+     */
+    private void eliminarElaboradorLista(ElaboradorEntity objetoElaborador) {
+        Iterator itr = lista.iterator();
+        while (itr.hasNext()) {
+            ElaboradorEntity metodoRecuperacionEntity = (ElaboradorEntity) itr.next();
+            if (metodoRecuperacionEntity.getIdElaborador() == objetoElaborador.getIdElaborador()) {
+                itr.remove();
+            }
+        }
+    }
+
+    /**
+     * Método que reinicia el objeto Aprobador Elaborador
+     */
+    public void nuevoElaboradorObjeto() {
+        objetoElaborador = new ElaboradorEntity();
+        UsuarioEntity usuario = new UsuarioEntity();
+        usuario.setIdUsuario(-1);
+        objetoElaborador.setUsuarioElaborador(usuario);
+    }
+
+    /**
+     * Método que evalua los accesos al formulario
+     */
+    public void permisos() {
+        ingresar = false;
+        actualizar = false;
+        eliminar = false;
+        ArrayList<MenuPermisosEntity> permisos = (ArrayList<MenuPermisosEntity>) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("menuLateral");
+        for (MenuPermisosEntity permisoObj : permisos) {
+            for (MenuPermisosEntity nivel1 : permisoObj.getSubNivel()) {
+                for (MenuPermisosEntity nivel2 : nivel1.getSubNivel()) {
+                    int idPer = (int) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("idPermiso");
+                    System.out.println("nn: " + nivel2.getNombrePermiso() + "-" + nivel2.getAsociadoMenu() + " - " + idPer);
+                    if (idPer == nivel2.getAsociadoMenu()) {
+                        switch (nivel2.getNombrePermiso()) {
+                            case "insert":
+                                ingresar = true;
+                                break;
+                            case "update":
+                                actualizar = true;
+                                break;
+                            case "delete":
+                                eliminar = true;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/java/co/com/siscomputo/administracion/logic/ElaboradorLogic.java
+++ src/java/co/com/siscomputo/administracion/logic/ElaboradorLogic.java
@@ -1,14 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.com.siscomputo.administracion.logic;
+
+import co.com.siscomputo.endpoint.Administacion;
+import co.com.siscomputo.endpoint.Administacion_Service;
+import co.com.siscomputo.endpoint.ElaboradorEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author LENOVO
  */
-public class ElaboradorLogic {
+public class ElaboradorLogic {private Administacion_Service webService;
+    private Administacion port;
+    private List elaborador;
     
+    /**
+     * Funcion con la cual inicializo el service y el port de los WebServices
+     */
+    public void webService() {
+        webService = new Administacion_Service();
+        port = webService.getAdministacionPort();
+    }
+
+    public List getElaborador() {
+        return elaborador;
+    }
+
+    public void setElaborador(List elaborador) {
+        this.elaborador = elaborador;
+    }
+    
+    /**
+     * Método que trae una lista de Aprobador Elaborador
+     * @return 
+     */
+    public ArrayList<ElaboradorEntity> listaElaborador(){
+        webService();
+        ArrayList<ElaboradorEntity> listaaux=new ArrayList<>();
+        ArrayList<Object> listaObjeto =(ArrayList<Object>) port.listaElaborador().getRetorna();
+        for(Object obj:listaObjeto){
+            ElaboradorEntity objectelaborador=(ElaboradorEntity) obj;
+            listaaux.add(objectelaborador);
+        }
+        return listaaux;
+    }
+/**
+     * Método que permite ingresar un Aprobador Elaboradornuevo
+     * @param objeto
+     * @return 
+     */
+    public ElaboradorEntity insertarElaborador(ElaboradorEntity objeto){
+        webService();
+        ElaboradorEntity dispoRta=null;        
+        try {
+            dispoRta = port.insertarElaborador(objeto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dispoRta;
+    }
+/**
+     * Método que permite actualizar un Aprobador Elaborador
+     * @param metodo
+     * @return 
+     */
+    public String actualizarElaborador(ElaboradorEntity objeto){
+        webService();
+        String rta="";
+        try {            
+            if (port.actualizarElaborador(objeto) != null) {
+                rta = "Ok";
+            } else {
+                rta = "Error";
+            }
+        } catch (Exception e) {
+            rta="Error";
+            e.printStackTrace();            
+        }
+        return rta;
+    }
+/**
+     * Método que trae un solo Aprobador Elaborador por ID
+     * @param elaborador
+     * @return 
+     */
+    public ElaboradorEntity elaboradorPorID(int elaborador){
+        webService();
+        return port.elaboradorPorId(elaborador);
+    }
 }
